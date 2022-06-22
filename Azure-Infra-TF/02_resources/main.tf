28533b66df86411efefa145ef56ac9d8070d45ff
Viktor Horvath <viktor_horvath2@epam.com>
2022-06-22 17:23:30 +0200

    bastion subnet renamed to AzureBastionSubnet

--- Azure-Infra-TF/02_resources/main.tf
+++ Azure-Infra-TF/02_resources/main.tf
@@ -60,11 +60,11 @@ module "vnet" {
   resource_group_name = var.RG
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
-  subnet_names        = ["prod", "bastion"]
+  subnet_names        = ["prod", "AzureBastionSubnet"]
 
   nsg_ids = {
     prod = azurerm_network_security_group.PROD.id
-    bastion = azurerm_network_security_group.bastion.id
+    AzureBastionSubnet = azurerm_network_security_group.bastion.id
   }
 
 
