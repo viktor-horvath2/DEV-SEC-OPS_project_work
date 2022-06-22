c2759ce0e541acc8412f715152fa4336eaa40ba6
Viktor Horvath <viktor_horvath2@epam.com>
2022-06-22 22:57:58 +0200

    adding initial policy resource to TF infra

---
+++ Azure-Infra-TF/02_resources/policy.tf
@@ -0,0 +1,29 @@
+variable "RG" {
+  type        = string
+}
+
+resource "azurerm_policy_definition" "allowed_location_check" {
+  name        = "only-deploy-in-westeurope"
+  policy_type = "Custom"
+  mode        = "All"
+
+  policy_rule = <<POLICY_RULE
+    {
+    "if": {
+      "not": {
+        "field": "location",
+        "equals": "westeurope"
+      }
+    },
+    "then": {
+      "effect": "audit"
+    }
+  }
+POLICY_RULE
+}
+
+resource "azurerm_resource_group_policy_assignment" "location_policy_to_rg" {
+  name                 = "allowed_location-policy-assignment"
+  resource_id          = var.RG.id
+  policy_definition_id = azurerm_policy_definition.allowed_location_check.id
+}
