3ae34683cd2003c70fe81a020c6d7d2b4a0faf36
Viktor Horvath <viktor_horvath2@epam.com>
2022-06-22 09:46:24 +0200

    exporting to config file instead of env_vars due to complexity

--- Azure-Infra-TF/01_remote_backend/export_env_vars.sh
+++ Azure-Infra-TF/01_remote_backend/export_env_vars.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-TF_VAR_RG=$(../../terraform show | egrep resource_group_name | tr -d '[:space:]' | tr '=' ' ' | awk '{print$2}' | sed "s/\"//g")
-TF_VAR_SA=$(../../terraform show | egrep storage_account_name | tr -d '[:space:]' | tr '=' ' ' | awk '{print$2}' | sed "s/\"//g")
+#TF_VAR_RG:
+echo $(../../terraform show | egrep resource_group_name | tr -d '[:space:]') >> ../02_resources/config.tfbackend
+#TF_VAR_SA:
+echo $(../../terraform show | egrep storage_account_name | tr -d '[:space:]') >> ../02_resources/config.tfbackend
 cd ../02_resources
-TF_VAR_CONT=$(cat ./variables.auto.tfvars | egrep cont | tr -d '[:space:]' | tr '=' ' ' | awk '{print$2}' | sed "s/\"//g")
-TF_VAR_KEY=$(cat ./variables.auto.tfvars | egrep key | tr -d '[:space:]' | tr '=' ' ' | awk '{print$2}' | sed "s/\"//g")
-export TF_VAR_SA
-export TF_VAR_RG
-export TF_VAR_CONT
-export TF_VAR_KEY
+#TF_VAR_CONT:
+echo $(cat ./variables.auto.tfvars | egrep cont | tr -d '[:space:]' | sed "s/backend_container/container_name/") >> ./config.tfbackend
+#TF_VAR_KEY:
+echo $(cat ./variables.auto.tfvars | egrep key | tr -d '[:space:]' | sed "s/backend_//") >> ./config.tfbackend
